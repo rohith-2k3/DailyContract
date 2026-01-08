dfa70d08591bad2b88fd9c6f34ff644671b287fa
rohith-2k3 <tiruveedhularohith4@gmail.com>
2026-01-09 00:17:49 +0530

    Jan 8

--- src/Arrays/problems/LC3289.java
+++ src/Arrays/problems/LC3289.java
@@ -1,28 +1,36 @@
 package Arrays.problems;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LC3289 {
     public static int[] getSneakyNumbers(int[] nums) {
-        Arrays.sort(nums);
-        int[] sneaky = new int[2];
-        Arrays.fill(sneaky,-1);
-        int check = -1;
-        System.out.println(Arrays.toString(nums));
-        for(int i =0;i<nums.length;i++){
-            if(nums[i]==check){
-                if(sneaky[0] == -1){
-                    sneaky[0]=check;
-                }
-                else {
-                    sneaky[1] = check;
-                    break;
-                }
+//        int[] freq = new int[nums.length];
+//        int[] sneaky = new int[2];
+//        int id = 0;
+//        for(int i: nums){
+//            freq[i]++;
+//            if(freq[i]==2){
+//                sneaky[id++]=i;
+//                if(id == 2) break;
+//            }
+//        }
+//        return sneaky;
+
+        Set<Integer> seen = new HashSet<>();
+        int[] result = new int[2];
+        int idx = 0;
+
+        for (int num : nums) {
+            if (!seen.add(num)) {
+                result[idx++] = num;
+                if (idx == 2) break;
             }
-            check=nums[i];
         }
-        return sneaky;
+        return result;
     }
+
     public static void main(String[] args) {
         System.out.println(Arrays.toString(getSneakyNumbers(new int[]{7,1,5,4,3,4,6,0,9,5,8,2})));
 
