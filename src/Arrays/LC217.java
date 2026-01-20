ffe7a308ccd001a3e1d9023b740bff2dae81f221
rohith-2k3 <tiruveedhularohith4@gmail.com>
2026-01-20 11:34:24 +0530

    Jan 19

---
+++ src/Arrays/LC217.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class LC217 {
+    public static boolean containsDuplicate(int[] nums) {
+//        Map<Integer,Boolean> check = new HashMap<>(nums.length);
+//        for(int i : nums){
+//            if(!check.containsKey(i)){
+//                check.put(i,true);
+//            }
+//            else{
+//                return true;
+//            }
+//
+//        }
+//        return false;
+        Set<Integer> check = new HashSet<>(nums.length);
+        for(int i : nums){
+            if(!check.add(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        System.out.println(containsDuplicate(new int[]{1,2,3,1}));
+        System.out.println(containsDuplicate(new int[]{1,2,3,4}));
+    }
+}
